9f6cb5ddfb5f21163fa447f1adc9fc048844a408
wanghuazhou <huazhou.whz@alibaba-inc.com>
2017-09-03 16:43:51 +0800

    初步完成 MNIST 的下载、解压以及解析工作

---
+++ app/src/main/java/io/whz/androidneuralnetwork/App.java
@@ -0,0 +1,10 @@
+package io.whz.androidneuralnetwork;
+
+import android.app.Application;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
